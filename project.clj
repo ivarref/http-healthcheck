95828c19904be0cc9f2cad97f6cb22d6366b111c
Ivar Refsdal <ivar.refsdal@nsd.no>
2019-07-26 11:53:20 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject no.nsd/http-healthcheck "0.1.0-SNAPSHOT"
+(defproject no.nsd/http-healthcheck "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
